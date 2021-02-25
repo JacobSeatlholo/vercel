5918086f7b1b7eb03327914225cf6e074b0a94c4
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-02-25 15:10:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     
-    <img id="SilverbananaLogo.png" src="SilverbananaLogo.png" alt="SilverbananaLogo.png" width="304" height="228">
-    
+      <img src=<a href="https://imgbb.com/"><img src="https://i.ibb.co/tJMF41D/Silverbanana-Logo-png.png" alt="Silverbanana-Logo-png" border="0" /></a> 
     <div className={styles.container}>
+    
       <Head>
         <title>Silver Banana Printing</title>
         <link rel="icon" href="/favicon.ico" />

0832467ea2c4a0622f42de8d41dbd882c2da2a78
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-02-25 14:41:32 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,9 +42,9 @@ export default function Home() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Contact us &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Reach out to one of our friendly staff.
             </p>
           </a>
         </div>

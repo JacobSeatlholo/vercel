b83cf49dd26050ecceb5db767dca2f8b3cc3cd85
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-02-25 15:53:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    
+    <img src="https://i.ibb.co/tJMF41D/Silverbanana-Logo-png.png" alt="Silverbanana-Logo-png" border="0" /></a> 
+    <div className={styles.container}>
+    
+    
     <div className={styles.container}>
       <Head>
         <title>Silver Banana Printing</title>

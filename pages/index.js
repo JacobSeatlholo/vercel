282eab9e63ced39f8cbf87fa78890ab2f9900fad
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-02-25 14:21:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Silver Banana Cape Town
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Cape Town Printing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
